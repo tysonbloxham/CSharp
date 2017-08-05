0ef84adbd0f092adfbab2169c51980cd95f35516
tysonbloxham <tyson.master.of.the.pork.cubes@gmail.com>
2017-08-05 17:42:19 -0600

    * Program.cs: removed name variable and get method for setting it
    * HelloWorld.userprefs: removed name variable and get method for setting it

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,9 +6,6 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Your name:");
-            string name = Console.ReadLine();
-
             Console.WriteLine("How much sleep did you get last night?");
 
             int hoursOfSleep = int.Parse(Console.ReadLine());
