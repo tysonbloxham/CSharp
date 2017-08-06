a249f4ce037a15bdd0538bb8c6c48d0acfb48aee
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-08-06 17:21:27 -0600

    Created test to demonstrate the difference between value types and reference variables.

--- Grades/Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -11,13 +11,23 @@ namespace Grades.Tests.Types
     public class ReferenceTypeTests
     {
         [TestMethod]
-        public void VariablesHoldAReference()
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+        [TestMethod]
+        public void GradeBookVariablesHoldAReference()
         {
             GradeBook g1 = new GradeBook();
             GradeBook g2 = g1;
 
+            g1 = new GradeBook();
             g1.Name = "Scott's grade book";
-            Assert.AreEqual(g1.Name, g2.Name);
+            Assert.AreNotEqual(g1.Name, g2.Name);
         }
     }
 }
