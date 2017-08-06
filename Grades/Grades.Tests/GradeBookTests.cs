8538a60d5064793019217d48f99dd7b55874edd5
Tyson Bloxham <tyson.master.of.the.pork.cubes@gmail.com>
2017-08-06 16:33:32 -0600

    Added tests for lowest and average grade

--- Grades/Grades.Tests/GradeBookTests.cs
+++ Grades/Grades.Tests/GradeBookTests.cs
@@ -12,7 +12,7 @@ namespace Grades.Tests
     public class GradeBookTests
     {
         [TestMethod]
-        public void ComputesHighestGrade()
+        public void ComputeHighestGrade()
         {
             GradeBook book = new GradeBook();
             book.AddGrade(10);
@@ -21,5 +21,30 @@ namespace Grades.Tests
             GradeStatistics result = book.ComputeStatistics();
             Assert.AreEqual(90, result.HighestGrade);
         }
+
+        [TestMethod]
+        public void ComputeLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(10, result.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComputeAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.AverageGrade, 0.01);
+        }
+
+
     }
 }
