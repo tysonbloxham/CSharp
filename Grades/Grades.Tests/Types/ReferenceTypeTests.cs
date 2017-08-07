5723651ccd85f2e93e9face28b38473a37a37e67
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-08-06 19:03:07 -0600

    Updated tests to pass by reference instead of by value

--- Grades/Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -14,12 +14,12 @@ namespace Grades.Tests.Types
         public void ValueTypesPassByValue()
         {
             int x = 46;
-            IncrementNumber(x);
+            IncrementNumber(ref x);
 
-            Assert.AreEqual(46, x);
+            Assert.AreEqual(47, x);
         }
 
-        private void IncrementNumber(int number)
+        private void IncrementNumber(ref int number)
         {
             number += 1;
         }
@@ -30,12 +30,13 @@ namespace Grades.Tests.Types
             GradeBook book1 = new GradeBook();
             GradeBook book2 = book1;
 
-            GiveBookAName(book2);
-            Assert.AreEqual("A GradeBook", book1.Name);
+            GiveBookAName(ref book2);
+            Assert.AreEqual("A GradeBook", book2.Name);
         }
 
-        private void GiveBookAName(GradeBook book)
+        private void GiveBookAName(ref GradeBook book)
         {
+            book = new GradeBook(); 
             book.Name = "A GradeBook";
         }
 
