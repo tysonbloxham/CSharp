f8a09398f47ad88fa1e1ac1d366d68fa46a33531
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-08-06 19:11:08 -0600

    Added tests to show that some data types are immutable, but can be reassigned to the same variable

--- Grades/Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -11,6 +11,24 @@ namespace Grades.Tests.Types
     public class TypeTests
     {
         [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "scott";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOTT", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+        [TestMethod]
         public void ValueTypesPassByValue()
         {
             int x = 46;
