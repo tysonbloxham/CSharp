98a4a02f99b17cfdc7b9028344cbb1f1404af56d
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-08-16 10:27:17 -0600

    Removed unneccesary using statements

--- Grades/Grades/NameChangedDelegate.cs
+++ Grades/Grades/NameChangedDelegate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Grades
+﻿namespace Grades
 {
     public delegate void NameChangedDelegate(object sender, NameChangedEventArgs args);
     
